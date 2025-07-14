5f52c037b9aa90d21acfffa7fa5f93d358ab4e8f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-14 22:11:48 +1000

    Update contact_form.py

--- components/contact_form.py
+++ components/contact_form.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def enquiry_form():
+    with st.form("enquiry_form"):
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+        message = st.text_area("Your Message or Query")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.success("Thanks! We'll get back to you shortly.")
 
