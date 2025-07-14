f5d595debb911f9d5e32b831c7d358ac970da904
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-14 22:15:45 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+CATEGORIES = [
+    "Old Age Support",
+    "Child Education",
+    "Girl Education",
+    "Animal Care",
+    "Poverty Relief"
+]
+
+EMAIL_FOR_CONTACT = "careconnect.support@email.com"
+DONATION_TARGET = 100000  # INR or any currency
 
